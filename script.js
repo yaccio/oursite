176a05ee30df539263fdcd6255d644d3a508cb8d
Anders <andersenghoej@gmail.com>
2016-04-12 16:15:09 +0200

    now the cases can be expanded to get more info

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 console.log("peace");
 
-function expandCase(){
+function expandCase1(){
   console.log("clicked");
 
   var el = document.getElementById('expanded-case');
     if(el) {
-      el.className == "black";
       el.className += el.className ? ' expanded-case' : 'expanded-case';
     }
+    document.getElementById("case1").style.display = 'block';
     document.getElementById("case2").style.display = 'none';
     document.getElementById("case3").style.display = 'none';
 }
+
+function expandCase2(){
+  console.log("clicked");
+
+  var el = document.getElementById('expanded-case');
+    if(el) {
+      el.className += el.className ? ' expanded-case' : 'expanded-case';
+    }
+    document.getElementById("case1").style.display = 'none';
+    document.getElementById("case2").style.display = 'block';
+    document.getElementById("case3").style.display = 'none';
+}
+
+function expandCase3(){
+  console.log("clicked");
+
+  var el = document.getElementById('expanded-case');
+    if(el) {
+      el.className += el.className ? ' expanded-case' : 'expanded-case';
+    }
+    document.getElementById("case2").style.display = 'none';
+    document.getElementById("case1").style.display = 'none';
+    document.getElementById("case3").style.display = 'block';
+}
+
+function closeCase(){
+  console.log("clicked");
+
+  var el = document.getElementById('expanded-case');
+    if(el) {
+      el.className = el.className.replace( /(?:^|\s)expanded-case(?!\S)/g , '' );
+    }
+}
+
+document.getElementById("caseLink1").addEventListener("click", expandCase1);
 
 
 $(function() {
@@ -21,7 +56,7 @@ $(function() {
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 500);
         return false;
       }
     }
